de8ee78c6096a4b0843a477f666dddf83df1ad93
shiromachi <mustard.dreieck@gmail.com>
2020-11-09 00:39:23 +0900

    画像ファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'filenotes#index'
+  devise_for :users
   resources :filenotes, only: [:new, :create, :destroy]
   resources :works, only: [:index, :new, :create, :edit, :update, :destroy]
 end
